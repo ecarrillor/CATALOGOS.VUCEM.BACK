e8d97971a3257f08a7fc2d3f371ed132cd3250b8
Evelyn Bahena <gilbahenaevelyn@gmail.com>
2026-02-26 23:05:24 -0600

    Correccion en select tipo tramite

--- vucem-catalogos-service/src/main/java/com/example/vucem_catalogos_service/persistence/repo/ICatTipoTramiteRepository.java
+++ vucem-catalogos-service/src/main/java/com/example/vucem_catalogos_service/persistence/repo/ICatTipoTramiteRepository.java
@@ -3,6 +3,7 @@ package com.example.vucem_catalogos_service.persistence.repo;
 import com.example.vucem_catalogos_service.model.entity.CatTipoTramite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,14 @@ public interface ICatTipoTramiteRepository extends JpaRepository<CatTipoTramite,
         JpaSpecificationExecutor<CatTipoTramite> {
 
 
+    @Query("""
+    SELECT t
+    FROM CatTipoTramite t
+    WHERE t.blnActivo = true
+      AND t.descModalidad IS NOT NULL
+      AND TRIM(t.descModalidad) <> ''
+    ORDER BY t.id DESC
+""")
     List<CatTipoTramite> findByBlnActivoTrueOrderByIdDesc();
 
     List<CatTipoTramite> findByBlnActivoTrue();
